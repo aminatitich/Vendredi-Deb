a2b3a6a832da598fdb58dc4fa0fda606d5316959
aminatitich <89085765+aminatitich@users.noreply.github.com>
2024-03-20 12:14:51 +0000

    Added the input line

---
+++ main.py
@@ -0,0 +1,85 @@
+import random 
+
+def mot_obtenu():
+    liste = ["Robot", "Programmation", "Pytho,", "Ordinateur","AI", "Java", "IT", "Computer Science"]
+    mot = random.choice(liste)
+    return mot
+
+def afficher_pendu(essaies):
+    phases = [
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    \ | /
+                   |      |
+                   |     / \
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    \ | /
+                   |      |
+                   |     
+                   -
+                """,
+                 """
+                   --------
+                   |      |
+                   |      O
+                   |    \ | /
+                   |      
+                   |     
+                   -
+                """,
+                 """
+                   --------
+                   |      |
+                   |      O
+                   |    \ | 
+                   |      
+                   |     
+                   -
+                """,
+                  """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                 """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """ ]
+    return phases[essaies]
+
+
+def play(mot):
+
+
+    essaies = 8
+    print("Jouons au pendu")
+    print(afficher_pendu(essaies))
+    mot_complet = "_"*len(mot)
+    guessed = True
+
+    guessed_mot = []
+    guessed_lettre = []
+
+    print(mot_complet)
+
+    while essaies > 0 and guessed == False:
+        guess = str(input("Veuillez saisir une lettre ou un mot: "))
+        
+
+
